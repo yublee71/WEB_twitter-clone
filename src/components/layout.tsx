aa6a4ba272ced2794cd02438203c0476a0549bf9
Yubeen Lee <lybin71@naver.com>
2025-05-30 00:08:38 +0100

    feat: add basic layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,99 @@
-import { Outlet } from "react-router";
+import { Link, Outlet, useNavigate } from "react-router";
+import styled from "styled-components";
+import { auth } from "../firebase";
 
+const Wrapper = styled.div`
+  display: grid;
+  gap: 50px;
+  grid-template-columns: 1fr 4fr;
+  height: 100px;
+  padding: 50px 0px;
+  width: 100%;
+  max-width: 860px;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+const MenuItem = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid white;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  svg {
+    width: 30px;
+    fill: white;
+  }
+  &.log-out {
+    border-color: tomato;
+    svg {
+      fill: tomato;
+    }
+  }
+`;
 export default function Layout() {
+  const navigate = useNavigate();
+  const logout = async () => {
+    const ok = confirm("Are you sure you want to log out?");
+    if (ok) {
+      auth.signOut();
+      navigate("/login");
+    }
+  };
   return (
-    <>
+    <Wrapper>
+      <Menu>
+        <MenuItem>
+          <Link to="/">
+            <svg
+              data-slot="icon"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path d="M8.543 2.232a.75.75 0 0 0-1.085 0l-5.25 5.5A.75.75 0 0 0 2.75 9H4v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1v-1a1 1 0 1 1 2 0v1a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V9h1.25a.75.75 0 0 0 .543-1.268l-5.25-5.5Z" />
+            </svg>
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/profile">
+            <svg
+              data-slot="icon"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+            </svg>
+          </Link>
+        </MenuItem>
+        <MenuItem className="log-out" onClick={logout}>
+          <svg
+            data-slot="icon"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M2 4.75A2.75 2.75 0 0 1 4.75 2h3a2.75 2.75 0 0 1 2.75 2.75v.5a.75.75 0 0 1-1.5 0v-.5c0-.69-.56-1.25-1.25-1.25h-3c-.69 0-1.25.56-1.25 1.25v6.5c0 .69.56 1.25 1.25 1.25h3c.69 0 1.25-.56 1.25-1.25v-.5a.75.75 0 0 1 1.5 0v.5A2.75 2.75 0 0 1 7.75 14h-3A2.75 2.75 0 0 1 2 11.25v-6.5Zm9.47.47a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 1 1-1.06-1.06l.97-.97H5.25a.75.75 0 0 1 0-1.5h7.19l-.97-.97a.75.75 0 0 1 0-1.06Z"
+            />
+          </svg>
+        </MenuItem>
+      </Menu>
       <Outlet />
-    </>
+    </Wrapper>
   );
 }
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,16 +1,7 @@
-import { auth } from "../firebase";
-import { useNavigate } from "react-router";
-
 export function Home() {
-  const navigate = useNavigate();
-  const logout = () => {
-    auth.signOut();
-    navigate("/login");
-  };
   return (
     <div>
       <h1>Home</h1>
-      <button onClick={logout}>Log Out</button>
     </div>
   );
 }
