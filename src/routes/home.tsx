6e450caa1e57aad21c612b250a05f91e2b6ce439
Yubeen Lee <lybin71@naver.com>
2025-05-29 16:28:23 +0100

    feat: redirect to login when sign out

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,8 +1,11 @@
 import { auth } from "../firebase";
+import { useNavigate } from "react-router";
 
 export function Home() {
+  const navigate = useNavigate();
   const logout = () => {
     auth.signOut();
+    navigate("/login");
   };
   return (
     <div>
