6c10530d7bfa70c41e39c858daa0cb157a275dea
Yubeen Lee <lybin71@naver.com>
2025-06-04 12:25:27 +0100

    feat: add realtime functionality using onSnapshot

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { db } from "../firebase";
 import { Tweet } from "./tweet";
+import type { Unsubscribe } from "firebase/auth";
 
 export interface ITweet {
   id: string;
@@ -13,21 +20,34 @@ export interface ITweet {
   createdAt: number;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 
 export function Timeline() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
-  const fetchTweets = async () => {
-    const q = query(collection(db, "tweets"), orderBy("createdAt", "desc"));
-    const querySnapshot = await getDocs(q);
-    const tweets = querySnapshot.docs.map((doc) => {
-      const { tweet, createdAt, userName, userId, photo } = doc.data();
-      return { tweet, createdAt, userName, userId, photo, id: doc.id };
-    });
-    setTweets(tweets);
-  };
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      const q = query(
+        collection(db, "tweets"),
+        orderBy("createdAt", "desc"),
+        limit(25)
+      );
+      unsubscribe = await onSnapshot(q, (snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, userName, userId, photo } = doc.data();
+          return { tweet, createdAt, userName, userId, photo, id: doc.id };
+        });
+        setTweets(tweets);
+      });
+    };
     fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
   return (
     <Wrapper>
